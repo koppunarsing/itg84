a9c1e7fc5d1268bcd5e18f9a537e46e85ddc3289
koppunarsing <koppunarsing@gmail.com>
2016-06-14 19:38:20 +0530

    Hai Hello how are

---
+++ src/Multiple.java
@@ -0,0 +1,14 @@
+
+public class Multiple {
+	
+		   public static void main(String args[]){
+		       int k;
+		       for(int i=1; i<=5; i++)
+		       {
+		           for(int j=1; j<=5; j++)
+		                
+		           System.out.println(i+"*"+j+"="+(i*j));
+		           
+		           }
+		       }
+		   } 
